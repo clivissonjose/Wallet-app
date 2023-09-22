d6bc9289107716988c0c7441bdfb9c53e6f29bf5
Clivissojose <cjbarbosa2001@gmail.com>
2023-09-22 14:28:49 -0300

    feat: integrate user initial validaion

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const onclicklogin = () => {
+ const validateUser = async (email) => {
+
+  try {
+    const result =  await fetch(
+        `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+        )
+
+   const user = await result.json()
+   
+   return user 
+
+  } catch (error) {
+      return { error }
+  }
+ }
+
+const onclicklogin = async () => {
     const email = document.getElementById('input-email').value  
     
     if( email.length < 5 || !email.includes("@")) {
         alert("Email inválido!")
-      return
+       return
     }
 
-localStorage.setItem("@WalletApp:UserEmail", email)
+const result = await validateUser(email)
+
+console.log(result)
+
+if(result.error) {
+     alert("Erro ao validar E-mail!")
+     return
+}
+
+localStorage.setItem("@WalletApp:UserEmail", result.email)
+localStorage.setItem("@WalletApp:UserName", result.name)
+localStorage.setItem("@WalletApp:UserId", result.id)
+
 window.open("./src/pages/home/index3.html", "_self")
     
   }
